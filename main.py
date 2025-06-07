d5b5b49b6596d51d43c9aacca3a72ad956d5088f
Deernode <99195911+Deernode@users.noreply.github.com>
2025-06-07 16:03:57 +0200

    defined functions for password generation

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 import tkinter as tk
 from tkinter import simpledialog
 
-# Create the main window
-root = tk.Tk()
-root.withdraw()  # Hide the main window
 
-# Ask for the password
-password = simpledialog.askstring("Password Required", "Please enter your password:", show='*')
+def generate_password():
+    pass
 
-# Display result (optional)
-if password != '' and password is not None:
-    print("Password entered:", password)
-else:
-    print("User canceled input.")
+
+def check_password():
+    # TODO: check for existing password
+    # generate_password()
+    pass
+
+
+def intialize_root():
+    pass
+
+
+# the program starts here
+if __name__ == "__main__":
+    # Create the main window
+    root = tk.Tk()
+    # Hide the main window
+    root.withdraw()
+
+    # Ask for the password
+    password = simpledialog.askstring("Password Required", "Please enter your password:", show='*')
+
+    # Display result (optional)
+    if password != '' and password is not None:
+        # check password
+        check_password()
+        print("Password entered:", password)
+        # show root window
+        intialize_root()
+    else:
+        print("User canceled input.")
